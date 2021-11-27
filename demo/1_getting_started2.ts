ae6097af03c995f12a8b44f63a6b8cf9285ea6a9
cuvar <luca-mueller@gmx.net>
2021-11-27 15:35:21 +0100

    fix: wrong run instructions

--- demo/1_getting_started2.ts
+++ demo/1_getting_started2.ts
@@ -1,6 +1,6 @@
 // reference: https://deno.land/manual@v1.15.3/linking_to_external_code/import_maps
-// cmd win: deno run --import-map=res\import_map.json .\getting_started.ts
-// cmd unixoid: deno run --import-map=res/import_map.json getting_started.ts
+// cmd win: deno run --import-map=res\import_map.json .\1_getting_started2.ts
+// cmd unixoid: deno run --import-map=res/import_map.json 1_getting_started2.ts
 
 // additional: deno run https://deno.land/std@0.113.0/examples/welcome.ts
 
